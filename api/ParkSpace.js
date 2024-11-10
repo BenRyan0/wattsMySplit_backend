f98822dffe40ee4210ecc1d4b8851689974818ec
BenRyan0 <benryanrinconada5@gmail.com>
2024-11-10 17:38:15 +0800

    parkspace INtegration

--- api/ParkSpace.js
+++ api/ParkSpace.js
@@ -10,6 +10,20 @@ const path = require('path')
 
 router.post('/parkSpace', async (req, res) => {
     let {  park_number,availability,ModTime } = req.body;
+    try {
+        console.log(req.body)
+        return res.status(200).json({
+            status: "Success",
+            message: "Slot Availability Modified "
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            status: "Failed",
+            message: "Slot Availability Modification Failed "
+        });
+        
+    }
 
     console.log(req.body)
 });
